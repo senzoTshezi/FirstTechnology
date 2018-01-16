42443139e19eca049578d4be15dfe8d370c1ffa6
Senzo <senzo@realtimesolutions.co.za>
2018-01-16 12:21:50 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Fist Technology';
+  msg = "";
+  myEvent(currentdatetime) {
+     currentdatetime= new Date();
+         this.msg = currentdatetime;
+      }
+  
 }
